b7cb204120a880cc2deee77785a7c91eb4e103a4
Vladimir Sofronov <narviii@gmail.com>
2022-03-16 17:16:18 -0700

    ...

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/narvi/Apps/ukraine2canada/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/narvi/Apps/ukraine2canada/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/narvi/Apps/ukraine2canada/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/narvi/Apps/ukraine2canada/src/css/custom.css'),
-];
