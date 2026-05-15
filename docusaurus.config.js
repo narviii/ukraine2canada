685ce8549499f5422107e12498ecc02825d50380
Vlad Sofronov <narviii@gmail.com>
2026-05-15 15:09:27 -0700

    Update footer to built by digiPROPS and link to digiprops.io

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,7 +124,7 @@ const config = {
 
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} UA to Canada. Built with magic by Vlad Sofronov.`,
+        copyright: `Copyright © ${new Date().getFullYear()} UA to Canada. built by <a href="https://digiprops.io">digiPROPS</a>.`,
       },
       prism: {
         theme: lightCodeTheme,
